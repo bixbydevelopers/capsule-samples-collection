b1d1fbf9adb7d5c6a0b1b0977a10a4e6dd28aeb0
Sarah Moliner-Roy <s.moliner@samsung.com>
2019-08-09 10:57:12 -0700

    Third pass

--- code/PrepareUserData.js
+++ code/PrepareUserData.js
@@ -1,10 +1,16 @@
-module.exports.function = function ($vivContext, previousUserData) {
-  if (previousUserData) {
-    previousUserData.state="5"
-    return previousUserData
-  } else {
-    return {
-      state: 0
-    }
+module.exports.function = function ($vivContext, previousUserData, boss, minions) {
+  // Use previous data or default init
+  const userData = previousUserData || { version: 1, minions: []}
+
+  // Example of replacing single property with a new one
+  if (boss) {
+    userData.boss = boss
   }
+
+  // Example of appending multiple values to a property
+  if (minions) {
+    userData.minions = userData.minions.concat(minions)
+  }
+
+  return userData
 }
