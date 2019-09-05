6241611b049954ebcb74e2f1bc5824a5be0f6639
Sarah Moliner-Roy <s.moliner@samsung.com>
2019-09-05 10:56:42 -0700

    Delete unused imports

--- user-data-persistence/code/DeleteUserData.js
+++ user-data-persistence/code/DeleteUserData.js
@@ -1,5 +1,3 @@
-var http = require('http')
-var properties = require('./lib/properties.js')
 var remoteDB = require('./lib/remoteDB.js')
 
 module.exports.function = function ($vivContext, userData) {
