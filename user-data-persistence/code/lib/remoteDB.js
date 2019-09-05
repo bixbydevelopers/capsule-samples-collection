2a76ba6dbcaff121cd97b7e49625c12ed21a81ea
Sarah Moliner-Roy <s.moliner@samsung.com>
2019-09-05 13:40:13 -0700

    Explain  and  mappings

--- user-data-persistence/code/lib/remoteDB.js
+++ user-data-persistence/code/lib/remoteDB.js
@@ -1,14 +1,8 @@
 /**  
- * Remote DB CRUD Interface for a Bixby UserData Structure
+ * Remote Database CRUD Interface for a Bixby UserData Structure
  * 
- * In Bixby, the UserData Structure looks like this:
- * {
- *   $id: <dbUserId>
- *   $type: <bixbyConceptType>
- *   <property1>: <bixbyConceptValue1>
- *   <property2>: <bixbyConceptValue2>
- *   ...
- * }
+ * The functions here pass the UserData back and forth between the remote DB and Bixby.
+ * In doing so, they perform the transformations required to map how the UserData is stored in each system.
  * 
  * In the remote DB Collection, a UserData Document looks like this:
  * {
@@ -21,6 +15,15 @@
  *   }
  * }
  * 
+ * In Bixby, the UserData Structure looks like this:
+ * {
+ *   $id: <dbUserId> // Convenience key in Bixby to store provider specific ids (see https://bixbydevelopers.com/dev/docs/dev-guide/developers/actions.js-actions#provider-specific-identifiers)
+ *   $type: <bixbyConceptType> // Bixby automaticaly adds this labeling to all Concepts, but we don't need to save it to the remote DB
+ *   <property1>: <bixbyConceptValue1>
+ *   <property2>: <bixbyConceptValue2>
+ *   ...
+ * }
+ * The bixbyUserId resides in `$vivContext.userId=<bixbyUserId>`, so we pass it in as a separate param.
  **/
 var http = require('http')
 var properties = require('./properties.js')
