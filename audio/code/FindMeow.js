533cd8b4eaa961e7256de060bb2018671c9b0d99
kangawoo44 <wkang@viv.ai>
2019-10-02 10:17:03 -0700

    return some function which evaluates to true or false

--- audio/code/FindMeow.js
+++ audio/code/FindMeow.js
@@ -7,13 +7,12 @@ module.exports.function = function findMeow(searchTerm) {
     if (searchTerm) {
         searchTerm = searchTerm.toLowerCase()
         meowAudioFound = meowAudio.audioItems.filter(function (audioItem) {
-            keysToSearchOn.some(function (key) {
+            return keysToSearchOn.some(function (key) {
                 return audioItem[key] && audioItem[key].toLowerCase().includes(searchTerm)
             })
         })
     } else {
         meowAudioFound = meowAudio.audioItems
     }
-
     return meowAudioFound
 }
