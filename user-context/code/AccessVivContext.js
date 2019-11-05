03d301cc3e0611e02b60c2a95a572b688d413155
Sarah Moliner-Roy <s.moliner@samsung.com>
2019-11-05 10:58:00 -0800

    VIV-30275 Update user context sample capsule

--- user-context/code/AccessVivContext.js
+++ user-context/code/AccessVivContext.js
@@ -1,11 +1,4 @@
-var console = require('console')
-// The $vivContext below is not part of your AccessVivContext.model.bxb,
-// but it will be provided as input to the accessVivContext function
-module.exports.function = function accessVivContext (dummyInput, $vivContext) {
-  
-  // In the Debug Console panel, this logs to 
-  // Actions -> vivContext.AccessVivContext -> functions -> vivContext.AccessVivContext -> logs
-  console.log($vivContext);
-  
-  return $vivContext.locale;
+// Request access to the $vivContext by declaring it as a parameter
+module.exports.function = function accessVivContext ($vivContext) {  
+  return JSON.stringify($vivContext, undefined, 1);
 }
