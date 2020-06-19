fe6218894c79312d94ad61b89471707824b90db2
Roger Kibbe <rkibbe@viv.ai>
2020-06-19 16:52:55 -0700

    add permissions sample

---
+++ permissions/code/FindLocation.js
@@ -0,0 +1,18 @@
+var console = require("console");
+
+module.exports.function = function findLocation (myLocation, profile) {
+    
+  //console.log ("myLocation = " + JSON.stringify(myLocation))
+  //console.log ("profile = " + JSON.stringify(profile))
+
+  // If either null, simple no-result shown. A production capsule would typically
+  // have more logic here
+  if (!myLocation || !profile) {
+    return null;
+  }
+
+  return {
+    myFullName: profile.structuredName,
+    myLocation: myLocation
+  }
+}
