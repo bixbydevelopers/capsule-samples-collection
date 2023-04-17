1bbc15d0973b17795592f3a484f3c5c7fc9e4cae
Felipe Gusmão <f.gusmao@samsung.com>
2023-04-17 12:01:05 -0300

    [input-forms] JSRS Migration

--- input-forms/code/findPersons.js
+++ input-forms/code/findPersons.js
@@ -1,22 +1,25 @@
 const people = [
-    {
-      firstName: "Joe",
-      lastName: "Smith",
-      gender: "Male"
-    },
-    {
-      firstName: "Meg",
-      lastName: "Ana",
-      gender: "Female"
-    }
-  ]
+  {
+    firstName: 'Joe',
+    lastName: 'Smith',
+    gender: 'Male',
+  },
+  {
+    firstName: 'Meg',
+    lastName: 'Ana',
+    gender: 'Female',
+  },
+];
 
-module.exports.function = function findPersons (searchTerm) {
+export default function findPersons({ searchTerm }) {
   if (!searchTerm) {
     return people;
   }
 
-  return people.filter(person => {
-    return person.firstName.toLowerCase().includes(searchTerm) || person.lastName.toLowerCase().includes(searchTerm)
+  return people.filter((person) => {
+    return (
+      person.firstName.toLowerCase().includes(searchTerm) ||
+      person.lastName.toLowerCase().includes(searchTerm)
+    );
   });
 }
