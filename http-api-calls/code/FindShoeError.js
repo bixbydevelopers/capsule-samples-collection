82182a3d034ee234e9f26d3574f20e6f1a840248
Woo Kang <woo.k@samsung.com>
2023-04-26 10:54:14 -0700

    mangle the url to get 404 error instead, change comments

--- http-api-calls/code/FindShoeError.js
+++ http-api-calls/code/FindShoeError.js
@@ -3,7 +3,7 @@ var console = require('console')
 var fail = require('fail')
 module.exports.function = function findShoeError () {
   try {
-    // This call will throw a 500, and the http library will throw a JavaScript error that you can catch.
+    // This call will throw a 404, and the http library will throw a JavaScript error that you can catch.
     var response = http.getUrl('https://my-json-server.typicode.com/bixbydevelopers/capsule-samples-collectio', { format: 'json' });
   } catch (e) {
     console.log(e);
--- http-api-calls/code/FindShoeReturnHeaders.js
+++ http-api-calls/code/FindShoeReturnHeaders.js
@@ -2,13 +2,13 @@ var http = require('http')
 var console = require('console')
 module.exports.function = function findShoeReturnHeaders () {
 
-  // This call will throw a 500, but since we are using returnHeaders: true,
+  // This call will throw a 404, but since we are using returnHeaders: true,
   // we must check for the error ourselves.
   var errorResponse = http.getUrl('https://my-json-server.typicode.com/bixbydevelopers/capsule-samples-collectio', { format: 'json', returnHeaders: true });
   
   console.log(errorResponse);
   
-  if (errorResponse.status === 500) {
+  if (errorResponse.status === 404) {
     console.log("There was an error.");
   }
 
